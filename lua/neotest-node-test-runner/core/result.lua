c1c967c64c8c6cbe5b31180d17af78b4be78290e
Nikita Elfimov <nelfimov@mail.ru>
2025-10-25 14:37:13 +0300

    feat(result): better error handling

--- lua/neotest-node-test-runner/core/result.lua
+++ lua/neotest-node-test-runner/core/result.lua
@@ -2,13 +2,17 @@ local async = require("neotest.async")
 
 local Result = {}
 
+local function strip_ansi(s)
+	return (s:gsub("\27%[[0-9;]*m", ""))
+end
+
 ---Process result file
 ---@param spec neotest.RunSpec
 ---@param result_output neotest.StrategyResult
 ---@param _ neotest.Tree
 ---@return neotest.Result
 function Result.results(spec, result_output, _)
-	if not type(result_output) == "table" or not result_output.output then
+	if type(result_output) ~= "table" or not result_output.output then
 		vim.notify("Unknown result_output structure: " .. vim.inspect(result_output), vim.log.levels.ERROR)
 		return {}
 	end
@@ -27,9 +31,10 @@ function Result.results(spec, result_output, _)
 	local errors = {}
 	local is_error = false
 	local error = {}
+	local is_error_message = false
 
 	for _, line in ipairs(file_content) do
-		if is_error == false then
+		if not is_error then
 			if line:match("# Subtest:") then
 				local padding, subtest_name = line:match("^(%s*)# Subtest: (.+)$")
 				local padding_length = padding and #padding or 0
@@ -39,7 +44,7 @@ function Result.results(spec, result_output, _)
 					context_stack[test_level] = subtest_name
 				end
 
-				if last_test_key and results[last_test_key] and #errors > 0 then
+				if last_test_key and #errors > 0 then
 					results[last_test_key].errors = errors
 					errors = {}
 				end
@@ -47,11 +52,16 @@ function Result.results(spec, result_output, _)
 				if #context_stack > test_level then
 					context_stack[#context_stack] = nil
 				end
-			elseif line:match("ok %d+") then
+			elseif line:match("ok %d+ %-") then
 				local test_indent, test_status, test_name = line:match("^(%s*)(%w*%s*ok) %d+ %- (.+)$")
 
 				if test_status and test_name then
-					local status = test_status == "ok" and "passed" or "failed"
+					local status = (test_status == "ok") and "passed" or "failed"
+
+					if status == "failed" then
+						is_error = true
+					end
+
 					local test_result_level = #test_indent / 4 + 1
 
 					local full_context = table.concat(context_stack, "::", 1, test_result_level)
@@ -63,27 +73,37 @@ function Result.results(spec, result_output, _)
 					}
 					last_test_key = test_key
 				end
-			elseif line:match("error: |-") then
-				is_error = true
 			end
 		end
 
-		if is_error == true then
-			if line:match("TestContext") and is_error then
-				local line_number = line:match("TestContext.*:(%d+):%d+%)")
-				error.line = line_number
-			elseif line:match("code: ") or line:match("%.%.%.") then
+		if is_error then
+			if line:match("%.%.%.") then
 				is_error = false
 				table.insert(errors, error)
 				error = {}
-			elseif not line:match("error: |-") then
-				local initial_message = ""
-				if error.message then
-					initial_message = error.message
+			elseif line:match("error: ") then
+				is_error_message = true
+				if line:match("error: |%-") then
+					goto continue
 				end
-				local new_message = line:match("^%s+(.+)")
-				error.message = initial_message .. "\n" .. new_message
+			elseif line:match("code: ") then
+				is_error_message = false
+			elseif line:match("^test at ") then
+				local _, line_number, _ = line:match("^test at ([^:]+):(%d+):(%d+)")
+				if line_number then
+					error.line = line_number
+				end
 			end
+
+			if is_error_message and is_error then
+				local new_message = line:match("^%s+(.+)")
+				if new_message then
+					new_message = strip_ansi(new_message)
+					error.message = (error.message or "") .. "\n" .. new_message
+				end
+			end
+
+			::continue::
 		end
 	end
 
